d7cc0b8a79f5b9213e12d8b18cb61982c09067ca
first3258 <73007435+first3258@users.noreply.github.com>
2021-05-13 18:16:19 +0700

    book.103

--- book-node/app.js
+++ book-node/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -49,6 +50,19 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.use((req, res, next) => {
+  const allowedOrigins = ['https://kaiju-book.azurewebsites.net/'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020/%27);
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
 });
 
 module.exports = app;
